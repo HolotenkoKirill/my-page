b3449d2bf25f488fe9655bc595ec244b18f18b7e
Holotenko Kirill <62765104+HolotenkoKirill@users.noreply.github.com>
2020-05-01 00:49:30 +0300

    Add files via upload

---
+++ javaskript.js
@@ -0,0 +1,40 @@
+function darkMode(){
+	var firstScreen = document.getElementById('Me');
+	var films = document.getElementById('bgfilms');
+	var stadium = document.getElementById('bgstadium');
+
+
+
+	firstScreen.style.background="linear-gradient(284.21deg, #149FDB 0%, #185BA4 31.86%, #1E57AD 51.04%, #1861A9 64.02%, #13A0DC 100%)";
+	films.style.background="linear-gradient(284.21deg, #149FDB 0%, #185BA4 31.86%, #1E57AD 51.04%, #1861A9 64.02%, #13A0DC 100%)";
+	stadium.style.background="linear-gradient(284.21deg, #149FDB 0%, #185BA4 31.86%, #1E57AD 51.04%, #1861A9 64.02%, #13A0DC 100%)";
+	document.body.style.background = "#110E33";
+	document.getElementById('connect').style.color = "#D4D4D4";
+	document.getElementById('itstime').style.color = "#D4D4D4";
+	document.getElementById('metal').style.color = "#D4D4D4";
+	document.getElementById('revolution').style.color = "#D4D4D4";
+	document.getElementById('family').style.color = "#D4D4D4";
+	document.getElementById('friends').style.color = "#D4D4D4";
+	document.getElementById('job').style.color = "#D4D4D4";
+
+
+}
+function lightMode(){
+	var firstScreen = document.getElementById('Me');
+	var films = document.getElementById('bgfilms');
+	var stadium = document.getElementById('bgstadium');
+
+
+
+	firstScreen.style.background="linear-gradient(295.65deg, #8E43FA 0%, #F3608B 51.56%, #FFA807 100%)";
+	films.style.background="linear-gradient(295.65deg, #8E43FA 0%, #F3608B 51.56%, #FFA807 100%)";
+	stadium.style.background="llinear-gradient(295.65deg, #8E43FA 0%, #F3608B 51.56%, #FFA807 100%)";
+	document.body.style.background = "#fff";
+	document.getElementById('connect').style.color = "#626262";
+	document.getElementById('itstime').style.color = "#626262";
+	document.getElementById('metal').style.color = "#626262";
+	document.getElementById('revolution').style.color = "#626262";
+	document.getElementById('family').style.color = "#626262";
+	document.getElementById('friends').style.color = "#626262";
+	document.getElementById('job').style.color = "#626262";
+}
